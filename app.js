bece2238771fb726d7c579da304e823ff3e6c5f8
Nyiema Bayfield <ntmbayfield@gmail.com>
2018-05-01 15:12:23 -0700

    add feature that allows user to tell which color is currently selected.

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+var colorsArray = ["red", "blue"];
+var brushColor = colorsArray[0];
+var currentColor;
 
 //Function that creates number x number grid of white tiles that have eventListener for click which turns background to red
 function makeTile(number) {
@@ -12,14 +15,31 @@ function makeTile(number) {
     tile.style.border = "dotted black 3px";
     document.body.appendChild(tile);
     tile.addEventListener('click', function(){
-      tile.style.background = "red";
+      tile.style.background = brushColor;
     });
   }
 }
 
+//Function that creates a div that lets the user know what color is currently selected
+function makeCurrentColorIcon() {
+  currentColor = document.createElement('div');
+  currentColor.style.width="10%";
+  currentColor.style.height="10%";
+  currentColor.style.float="left";
+  currentColor.style.paddingBottom="5%";
+  currentColor.style.paddingRight ="5%";
+  currentColor.style.paddingLeft="5%";
+  currentColor.style.border = "solid yellow 3px";
+  currentColor.style.borderRadius = "10px";
+  currentColor.style.background="white";
+  document.body.appendChild(currentColor);
+
+}
+
+
 //Write function to create two circles below the canvas, each filled with a diffrent color
-function makeColorSelectors(colorsArr) {
-  for (let i=0; i < colorsArray.length; i++) {
+function makeColorPalette(colorsArr) {
+  for (let i=0; i < colorsArr.length; i++) {
     let colorCircle = document.createElement('div');
     colorCircle.style.width="10%";
     colorCircle.style.height="10%";
@@ -29,10 +49,18 @@ function makeColorSelectors(colorsArr) {
     colorCircle.style.border = "solid black 3px";
     colorCircle.style.borderRadius = "10px";
     document.body.appendChild(colorCircle);
+    colorCircle.addEventListener('click', function(){
+      brushColor = colorsArr[i];
+      console.log("brush color is now ", colorsArr[i]);
+      currentColor.style.background=colorsArr[i];
+    });
   }
 }
 
-makeTile(2);
-let colorsArray = ["red", "blue"];
 
-makeColorSelectors(colorsArray);
+
+makeTile(2);
+
+makeCurrentColorIcon();
+
+makeColorPalette(colorsArray);
